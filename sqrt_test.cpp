77815b40286bbde563bdff4c8a9c8ef7c3061911
Lukas <lukasbownik99@gmail.com>
2017-10-30 21:26:55 +0100

    Update sqrt_test.cpp

--- sqrt_test.cpp
+++ sqrt_test.cpp
@@ -1,84 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+#include <iostream> 
+#include <cmath> 
 
-/* 
- * File:   main.c
- * Author: obsidiam
- *
- * Created on October 21, 2017, 7:31 PM
- */
-
-
-#include <cmath>
-#include <cstdlib>
-#include <iostream>
-
-float sqrt(float x){
+float sqrtf(float x){       
     float a = x, b = 1, result = x, epsilon = 0.0000001f;
+    while(true){ 
+        result = (result + b)/2.f;
+        b = a/result; //std::cout<<a<<std::endl;
+        if((result - b) < epsilon) break; 
+    } 
+    return b; 
+} 
+
+float function3(float x){ 
+    return -pow((0.5f*x),2) + 4.f; 
+}
+
+float function2(float x){ 
+    float fr = 4.f - (x * x); 
+    std::cout<<fr<<std::endl;
+    return sqrt(fr); 
+} 
+
+float function1(float x){ 
+    double fr = 4.f - (x * x); 
+    std::cout<<fr<<std::endl;
+    return -(sqrt(fr)); 
+} 
+
+float delta(float x, float x2, float n){
+    if(x > x2) return 0; return (x2 - x)/n; 
+}
+
+float trapeze_field(float a, float b, float h){
+    return ((a + b)/2.f) * h; 
+} 
+
+float rect_field(float a, float b){
+    return a * b; 
+}
+
+int main() {
+    float x, x2, n, a, b, tra_result = 0,rect = 0,deltares; 
+    std::cin>>x; 
+    std::cin>>x2; 
+    std::cin>>n; 
+    deltares = delta(x,x2,n); 
+    std::cout<<"Delta = "<<deltares<<std::endl; 
+    //std::cout<<sqrtf(1.44f)<<std::endl; 
     
-    while(true){
-        result = (result + b)/2;
-        b = a/result;
-        if((result - b) < epsilon) break;
-    }
-    return b;
-}
-
-float cubrt(float x){
-    float a = x, b = 1, c = 1, result = x;
+    for(float i = x; i < (x2 - deltares); i += deltares){ 
+        float y1 = function2(i); 
+        float y2 = function1(i); 
+        float y3 = function2(i + deltares); 
+        float y4 = function1(i + deltares); 
+        a = y1 - y2; 
+        b = y3 - y4; 
+        //std::cout<<y3<<std::endl; 
+        tra_result += trapeze_field(a,b,deltares);
+        rect += rect_field(a,deltares); 
+    } 
     
-    while(true){
-        result =  (result + b + c)/3;
-        b = result;
-        c = (a - b)/ result ;
-        if((b - c) < 0.00000001f) break;
-    }
-    
-    return c;
+    std::cout<<"Pole(trapezami) = "<<tra_result<<std::endl; std::cout<<"Pole(prostokatami) = "<<rect<<std::endl; 
+    return 0; 
 }
-
-float field(float a, float b,float deltares){
-    return ((a + b)/2) * deltares;
-}
-
-float function2(float x){
-    return (-pow(x,2) + 4);
-}
-
-float function(float x){
-    return (pow(x,2) - 4);
-}
-
-float delta(float x1, float x2, float n){
-    if(x1 > x2) return 0;
-    
-    return (x2 - x1)/n;
-}
-
-int main(int argc, char** argv) {
-    float x1,x2,n,a = 0,b = 0;
-    std::cin>>x1;
-    std::cin>>x2;
-    std::cin>>n;
-    
-    float deltares = delta(x1,x2,n);
-    std::cout<<deltares<<std::endl;
-    float result = 0;
-    
-    for(float i = x1; i < x2; i += deltares){
-        a = function2(i)  - function(i);
-        b = function2(i + deltares) - function(i + deltares);
-        result += field(a,b,deltares);
-    }
-    std::cout<<result<<std::endl;
-/*
-    float num;
-    std::cin>>num;
-    std::cout<<cubrt(num)<<std::endl;
-*/
-    return 0;
-}
-
